9adee51849e8a19b6649bf444559f54068055a09
patlollaalekya <patlollaalekhyareddy9@gmail.com>
2017-07-15 22:20:55 +0530

    childassessment details

---
+++ childassessement.sql
@@ -0,0 +1,13 @@
+ CREATE TABLE `childassess` (
+`name` varchar(11) NOT NULL,
+`gender` varchar(11) NOT NULL,
+`tname` varchar(11),
+`actual_age` int,
+`expected_age` int,
+`q1` int,
+`q2` int,
+`q3` int,
+`q4` int,
+`q5` int,
+`q6` int
+) ENGINE=InnoDB;
