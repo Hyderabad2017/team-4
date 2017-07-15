51e4c35c3b13e48592918ae585f36a629a6b6f25
srikanthsista304@gmail.com <srikanthsista304@gmail.com>
2017-07-15 18:30:16 +0530

    updated schema

---
+++ lc2.sql
@@ -0,0 +1,96 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 15, 2017 at 02:31 PM
+-- Server version: 10.1.24-MariaDB
+-- PHP Version: 7.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `lc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dstudent`
+--
+
+CREATE TABLE `dstudent` (
+  `stuid` varchar(20) NOT NULL,
+  `sname` varchar(20) NOT NULL,
+  `sgender` varchar(20) NOT NULL,
+  `sage` int(20) NOT NULL,
+  `sps` varchar(20) NOT NULL,
+  `sfamilytype` varchar(20) NOT NULL,
+  `ssiblings` int(20) NOT NULL,
+  `soob` int(20) NOT NULL,
+  `spedul` varchar(20) NOT NULL,
+  `sfamin` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ngo`
+--
+
+CREATE TABLE `ngo` (
+  `name` varchar(20) NOT NULL,
+  `eid` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `phno` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teacher`
+--
+
+CREATE TABLE `teacher` (
+  `name` varchar(20) NOT NULL,
+  `eid` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `phno` int(20) NOT NULL,
+  `tid` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `dstudent`
+--
+ALTER TABLE `dstudent`
+  ADD PRIMARY KEY (`stuid`);
+
+--
+-- Indexes for table `ngo`
+--
+ALTER TABLE `ngo`
+  ADD PRIMARY KEY (`eid`);
+
+--
+-- Indexes for table `teacher`
+--
+ALTER TABLE `teacher`
+  ADD PRIMARY KEY (`tid`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
