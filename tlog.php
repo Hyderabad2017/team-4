4b5848dfb4abac907661885b8672aaa393e6e2d5
Soundarya Malyala <hymasoundarya@gmail.com>
2017-07-15 21:22:46 +0530

    Add files via upload

--- tlog.php
+++ tlog.php
@@ -5,17 +5,16 @@ $password=$_POST['pwd'];
 $conn=mysqli_connect("localhost","root","") or die("couldnt connect".mysqli_error());
 mysqli_select_db($conn,"lc") or die("couldnt select database".mysqli_error());
 $res=mysqli_query($conn,"select * from teacher where eid='$emailid' and password='$password'");
-
+echo "hi";
 if($row=mysqli_fetch_array($res))
 {
 		header("location:home_member.php");
-		
+		echo "hi hello";
+
 }
 else
 {
-	//echo '<script>alert("Oops! Invalid Username or Password");</script>';
-	//header("location:index.php");
-	header("location:index.php?");
+	echo '<script>alert("Oops! Invalid Username or Password");</script>';
 }
 
 
