88d7d025c375f5da6f4cca0726a3b29f4150ae31
srikanthsista <srikanthsista304@gmail.com>
2017-07-15 19:36:46 +0530

    changed phone no data type
    now we can insert proper phone numbers

--- lc.sql
+++ lc.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 15, 2017 at 01:50 PM
+-- Generation Time: Jul 15, 2017 at 04:03 PM
 -- Server version: 10.1.24-MariaDB
 -- PHP Version: 7.1.6
 
@@ -25,6 +25,46 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `dstudent`
+--
+
+CREATE TABLE `dstudent` (
+  `stuid` varchar(20) NOT NULL,
+  `sname` varchar(20) NOT NULL,
+  `sgender` varchar(20) NOT NULL,
+  `sage` int(20) NOT NULL,
+  `sps` varchar(20) NOT NULL,
+  `sfamilytype` varchar(20) NOT NULL,
+  `ssiblings` int(20) NOT NULL,
+  `soob` int(20) NOT NULL,
+  `spedul` varchar(20) NOT NULL,
+  `sfamin` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ngo`
+--
+
+CREATE TABLE `ngo` (
+  `name` varchar(20) NOT NULL,
+  `eid` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `phno` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `ngo`
+--
+
+INSERT INTO `ngo` (`name`, `eid`, `password`, `phno`) VALUES
+('srikanth', '', '', 0),
+('a', 'a', 'a', 1234567890);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `teacher`
 --
 
@@ -32,13 +72,25 @@ CREATE TABLE `teacher` (
   `name` varchar(20) NOT NULL,
   `eid` varchar(20) NOT NULL,
   `password` varchar(20) NOT NULL,
-  `phno` int(20) NOT NULL,
+  `phno` bigint(20) NOT NULL,
   `tid` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `dstudent`
+--
+ALTER TABLE `dstudent`
+  ADD PRIMARY KEY (`stuid`);
+
+--
+-- Indexes for table `ngo`
+--
+ALTER TABLE `ngo`
+  ADD PRIMARY KEY (`eid`);
 
 --
 -- Indexes for table `teacher`
