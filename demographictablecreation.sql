18ed96611d4557776b8524068997c286f44a03ee
patlollaalekya <patlollaalekhyareddy9@gmail.com>
2017-07-15 22:17:55 +0530

    demographicdetails

---
+++ demographictablecreation.sql
@@ -0,0 +1,8 @@
+ CREATE TABLE `demographic` (
+`name` varchar(11) NOT NULL,
+`age` varchar(11) NOT NULL,
+`gender` varchar(11) NOT NULL,
+`parentalstatus` varchar(10),
+`occupation` varchar(10),
+`income` float
+) ENGINE=InnoDB;
